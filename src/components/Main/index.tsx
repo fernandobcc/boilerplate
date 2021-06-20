4dfa010fb91bb00ec58ee0cfe05b7f71e52f68f5
fernandobcc <fernandoxico@gmail.com>
2021-06-19 21:27:08 -0300

    cCreate first component and update tests

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Image de um átomo e react avançado escrito ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS e Style Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Desenvolvedor de frente para uma tela de computador"
+    />
+  </S.Wrapper>
 )
 
 export default Main
